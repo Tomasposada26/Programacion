be920efd46ecf6cf086aa066c92fa253c14006e5
Tomasposada26 <tomasposadas1@eafit.edu.co>
2025-09-09 12:40:33 -0500

    chore: elimina urls locales de frontend y backend (CORS)

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,5 +1,5 @@
 // Configuración de CORS
 module.exports = {
-  origin: [process.env.FRONTEND_URL || 'https://programacion-tau.vercel.app', 'http://localhost:3000'],
+  origin: process.env.FRONTEND_URL || 'https://programacion-tau.vercel.app',
   credentials: true
 };
